19c55fb34b92ddad398c98fa2b291303fc727733
lmmurry <67395023+lmmurry@users.noreply.github.com>
2022-03-29 18:10:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from Crypto.Cipher import AES
 import sys
+from Crypto.Random import get_random_bytes
 
-#************Methods below are just a sort of outline that I was thinking about
+
+# ************Methods below are just a sort of outline that I was thinking about
 def writeStringToFile(string, outputFile):
     file = open(outputFile, "w")
     file.write(string)
@@ -27,7 +29,7 @@ def readFileString(file):
 
 def readFileBytes(file):
     try:
-        with open(file) as f:
+        with open(file, "rb") as f:
             line = f.read()
             line = line.lower()
             return line
@@ -36,34 +38,48 @@ def readFileBytes(file):
         print("Could not open file")
 
 
-def encrypt(plainTextFile, keyFile, cipherText):
-    plainTextFile = plainTextFile.read()
-    plainTextFile.close()
-    cipherText = AES.new(key, AES.MODE_EAX)##### just added this to see if this would work also.
+def encrypt(plainTextFile, keyFile, cipherTextFile):
+    plaintext = readFileBytes(plainTextFile)  # read plaintext as binary
+    key = readFileBytes(keyFile)  # read keyFile to get key
+    cipher = AES.new(key, AES.MODE_EAX)  # create cipher with key
+    cipherText = cipher.encrypt(plaintext)  # encrypt plaintext and get cipher text
+    writeBytesToFile(cipherText, cipherTextFile)  # write ciphertext to cipherTextFile
+
     print("This is where encryption will happen")
-    #print("plainTextFile=", plainTextFile)
-    #print("keyFile=", keyFile)
-    #print("cipherText=", cipherText)# should this part not be included in the encrypt but only the decrypt function, 
-                                    #since the encrption is for the plaintext? just asking
+    print("plainText=", plainTextFile)
+    print("keyFile=", keyFile)
+    print("cipherText=", cipherTextFile)
 
 
-def decrypt(cipherText, keyFile, plainTextFile):
+def decrypt(cipherText, keyFile, decryptFile):
+    key = readFileBytes(keyFile)  # read keyFile to get key
+    cipher = AES.new(key, AES.MODE_EAX)  # create cipher with key
+    cipherText = readFileBytes(cipherText)  # encrypt plaintext and get cipher text
+    plaintext= cipher.decrypt(cipherText)
+    writeBytesToFile(plaintext, decryptFile)  # write ciphertext to cipherTextFile
+    
+    
+    
     print("This is where decryption will happen")
     print("cipherText=", cipherText)
     print("keyFile=", keyFile)
-    print("plainTextFile=", plainTextFile)#should this part not be included in the decrypt but only the encrypt function,
-                                  #since the decryption is for the ciphertext? just asking
+    print("plainText=", plainText)
+
+
 def generateKey(keySize, keyFile):
     print("This is where the key will be generated")
     print("keySize=", keySize)
     print("keyFile=", keyFile)
+    keySize = int(keySize)
+    key = get_random_bytes(keySize)
+    writeBytesToFile(key, keyFile)
 
 
 if __name__ == '__main__':
     if sys.argv[1] == '-e':
-        #arg 2 is the file containing the plain text/the text to be encrypted
-        #arg 3 is the file containing the key that was generated
-        #arg 4 is the file the encrypted text will be stored in
+        # arg 2 is the file containing the plain text/the text to be encrypted
+        # arg 3 is the file containing the key that was generated
+        # arg 4 is the file the encrypted text will be stored in
         #
         try:
             encrypt(sys.argv[2], sys.argv[3], sys.argv[4])
@@ -79,10 +95,12 @@ if __name__ == '__main__':
             decrypt(sys.argv[2], sys.argv[3], sys.argv[4])
         except ValueError:
             print("Illegal entry")
+        except TypeError:
+            print("Illegal entry")
 
     elif sys.argv[1] == '-g':
-        #arg 2 is the keysize
-        #argument 3 is the file the key is stores in
+        # arg 2 is the keysize
+        # argument 3 is the file the key is stores in
         #
         try:
             generateKey(sys.argv[2], sys.argv[3])
@@ -92,8 +110,3 @@ if __name__ == '__main__':
 
     else:
         print("Illegal entry")
-
-
-
-
-
