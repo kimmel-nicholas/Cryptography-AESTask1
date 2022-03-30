a1abd2989aba1d8f078bbea566e9e580f137bcbd
kimmel-nicholas <70000680+kimmel-nicholas@users.noreply.github.com>
2022-03-29 23:54:23 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from Crypto.Cipher import AES
 import sys
+from Crypto.Random import get_random_bytes
 
-#************Methods below are just a sort of outline that I was thinking about
+
+
 def writeStringToFile(string, outputFile):
     file = open(outputFile, "w")
     file.write(string)
     file.close()
+
+
+def convertBytesToString(bytesToConvert): 
+    newString = bytesToConvert.decode("utf-8")
+    return newString
 
 
 def writeBytesToFile(string, outputFile):
@@ -22,53 +29,58 @@ def readFileString(file):
             return line
 
     except FileNotFoundError:
-        print("Could not open file")
+        print("Could not open file string", file)
 
 
 def readFileBytes(file):
     try:
-        with open(file) as f:
+        with open(file, "rb") as f:
             line = f.read()
             line = line.lower()
             return line
 
     except FileNotFoundError:
-        print("Could not open file")
+        print("Could not open file bytes", file)
 
 
-def encrypt(plainText, keyFile, cipherText):
-    # plaintext = plaintext.read()
-    #plaintext.close()
-    #cipherText = AES.new(key, AES.MODE_EAX)##### just added this to see if this would work also.
-    print("This is where encryption will happen")
-    print("plainText=", plainText)
-    print("keyFile=", keyFile)
-    print("cipherText=", cipherText)# should this part not be included in the encrypt but only the decrypt function, 
-                                    #since the encrption is for the plaintext? just asking
+def encrypt(plainTextFile, keyFile, cipherTextFile):
+    plaintext = readFileBytes(plainTextFile)            # read plaintext as binary
+    key = readFileBytes(keyFile)                        # read keyFile to get key
+    cipher = AES.new(key, AES.MODE_EAX)                 # create cipher with key
+    cipherText = cipher.encrypt(plaintext)              # encrypt plaintext and get cipher text
+    print(cipherText)
+    writeBytesToFile(cipherText, cipherTextFile)        # write ciphertext to cipherTextFile
 
 
-def decrypt(cipherText, keyFile, plainText):
-    print("This is where decryption will happen")
-    print("cipherText=", cipherText)
-    print("keyFile=", keyFile)
-    print("plainText=", plainText)#should this part not be included in the decrypt but only the encrypt function,
-                                  #since the decryption is for the ciphertext? just asking
+def decrypt(cipherTextFile, keyFile, plainTextFile):
+    ciphertext = readFileBytes(cipherTextFile)
+    key = readFileBytes(keyFile)
+    cipher = AES.new(key, AES.MODE_EAX)
+    plainText = cipher.decrypt(ciphertext)
+    plainText = convertBytesToString(plainText)         #next thing to do is figure out how to convert the bytes/hex back into actual readable text
+    print(plainText)
+    writeStringToFile(plainText, plainTextFile)
+
+
+
 def generateKey(keySize, keyFile):
-    print("This is where the key will be generated")
-    print("keySize=", keySize)
-    print("keyFile=", keyFile)
+    keySize = int(keySize)/8        #The user enters the keysize which for AES is 128, 196, or 256. Since we're working with bytes I divide that number by 8
+
+    key = get_random_bytes(keySize)
+    print(key)
+    writeBytesToFile(key, keyFile)
 
 
 if __name__ == '__main__':
     if sys.argv[1] == '-e':
-        #arg 2 is the file containing the plain text/the text to be encrypted
-        #arg 3 is the file containing the key that was generated
-        #arg 4 is the file the encrypted text will be stored in
+        # arg 2 is the file containing the plain text/the text to be encrypted
+        # arg 3 is the file containing the key that was generated
+        # arg 4 is the file the encrypted text will be stored in
         #
         try:
             encrypt(sys.argv[2], sys.argv[3], sys.argv[4])
-        except ValueError:
-            print("Illegal entry")
+        except ValueError as e:
+            print("Illegal entry attempting to encrypt:", e)
 
     elif sys.argv[1] == '-d':
         # arg 2 is the file containing the text to be decrypted/the cipher text
@@ -77,21 +89,24 @@ if __name__ == '__main__':
         #
         try:
             decrypt(sys.argv[2], sys.argv[3], sys.argv[4])
-        except ValueError:
-            print("Illegal entry")
+        except ValueError as e:
+            print("Illegal entry attempting to decrypt:", e)
+        except TypeError as e:
+            print("Illegal entry attempting to decrypt:", e)
 
     elif sys.argv[1] == '-g':
-        #arg 2 is the keysize
-        #argument 3 is the file the key is stores in
+        ####need to check for valid key sizes: 128, 196, 256
+        # arg 2 is the keysize
+        # arg 3 is the file the key is stores in
         #
         try:
             generateKey(sys.argv[2], sys.argv[3])
 
         except ValueError:
-            print("Illegal entry")
+            print("Illegal entry when attempting to generate key")
 
     else:
-        print("Illegal entry")
+        print("Incorrect entry")
 
 
 
